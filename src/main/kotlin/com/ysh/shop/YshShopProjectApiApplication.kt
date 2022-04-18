25db8489cab7d693ea1503bab591c5bbc1a8e6c1
Sanha Gu <9sanha@gmail.com>
2022-04-18 11:29:37 +0900

    Feat: 프로젝트 세팅

---
+++ src/main/kotlin/com/ysh/shop/YshShopProjectApiApplication.kt
@@ -0,0 +1,13 @@
+package com.ysh.shop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YshShopProjectApiApplication
+
+fun main(args: Array<String>) {
+	println("sdfe");
+	runApplication<YshShopProjectApiApplication>(*args)
+
+}
---
+++ src/test/kotlin/com/ysh/shop/YshShopProjectApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ysh.shop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YshShopProjectApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
